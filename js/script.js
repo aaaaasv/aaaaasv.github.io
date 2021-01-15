0cc1ffc16b2c05eb210ac77973b274848f97eeb3
Andriy Aleksandrov <38463115+aaaaasv@users.noreply.github.com>
2021-01-15 18:30:54 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,77 @@
+$(document).ready(function() {
+  var filterBlock = $('.filter');
+  var filterExtended = $('.filter-extended')
+  var filterShown = false;
+
+  function hoverFunction() {
+    if (!filterShown) {
+      filterBlock.hide();
+      filterExtended.show();
+      filterShown = true;
+    } else {
+      filterBlock.show();
+      filterExtended.hide();
+      filterShown = false;
+    }
+  }
+  filterBlock.on('mouseover', hoverFunction);
+  filterExtended.on('mouseleave', hoverFunction);
+
+
+  function removeFilter() {
+    $('.design-item').addClass('carousel-item').show();
+    $('.backend-item').addClass('carousel-item').show();
+  }
+  $('.design-filter').click(function() {
+    if ($(this).text() === "X") {
+      $('.design-filter').text("DESIGN");
+      removeFilter();
+      return;
+    }
+    $('.backend-item').removeClass('carousel-item').hide();
+    $('.design-item').addClass('carousel-item').show();
+    $(this).text("X");
+  })
+
+  $('.backend-filter').click(function() {
+    if ($(this).text() === "X") {
+      $(this).text("BACKEND");
+      removeFilter();
+      return;
+    }
+    $('.backend-item').addClass('carousel-item').show();
+    $('.design-item').removeClass('carousel-item').hide();
+    $(this).text("X");
+
+
+  })
+
+const patternAnimationSpeed = 100
+var mathBrainPattern = $('.math-brain-section');
+var designBrainPattern = $('.design-brain-section');
+
+function activateDesignBrain() {
+  designBrainPattern.show("scale",{},patternAnimationSpeed);
+
+}
+function deactivateDesignBrain() {
+  designBrainPattern.hide("scale", {}, patternAnimationSpeed);
+}
+
+$('.design-brain').on('mouseenter', activateDesignBrain);
+$('.design-brain').on('mouseleave', deactivateDesignBrain);
+
+
+function activateMathBrain() {
+  mathBrainPattern.show("scale",{},patternAnimationSpeed);
+}
+function deactivateMathBrain() {
+  mathBrainPattern.hide("scale", {}, patternAnimationSpeed);
+}
+
+$('.math-brain').on('mouseenter', activateMathBrain);
+$('.math-brain').on('mouseleave', deactivateMathBrain);
+
+
+
+})
